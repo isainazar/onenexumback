98822473628ff87ee2d99af2fd401ac7229c2535
Iesc Hub <blaksleyn@gmail.com>
2022-11-07 14:51:49 -0300

    daily controller

--- controller/userController.js
+++ controller/userController.js
@@ -87,12 +87,15 @@ const createUser = async (req, res, next) => {
         gender: genderE.encryptedData,
         user_type,
       });
-      res.cookie("id_user", user.dataValues.id_user, {
-        /*  maxAge: 5000,
-        expires: new Date("01 12 2021"), */
+      res.cookie(
+        "id_user",
+        user.dataValues.id_user /*  {
+        maxAge: 5000,
+        expires: new Date("01 12 2021"),
         secure: false,
         httpOnly: true,
-      });
+      } */
+      );
       // generamos el payload/body para generar el token
       if (!user) {
         return res
