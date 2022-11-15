b6de93112a62fb0dcfab1a7da5501308dab346ad
Iesc Hub <blaksleyn@gmail.com>
2022-11-15 11:52:04 -0300

    Session y cookies

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ var app = express();
 
 app.use(
   cors({
-    origin: [`${URL}`],
-    methods: ["GET", "POST", "PUT"],
+    /*   origin: [`${URL}`],
+    methods: ["GET", "POST", "PUT"], */
     credentials: true,
   })
 );
@@ -44,7 +44,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", `${URL}`);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Authorization, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, X-Login, X-Date, X-Trans-Key, X-Content-Type, X-Version, Set-Cookie, set-Cookie"
