b5ada5ba6a248c37b4addcd17da55186eff32614
Iesc Hub <blaksleyn@gmail.com>
2022-11-15 12:15:33 -0300

    Session y cookies

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: `https://test.onenexum.com`,
     methods: ["GET", "POST", "PUT"],
-    credentials: true,
+    // credentials: true,
   })
 );
 app.use(cookieParser());
