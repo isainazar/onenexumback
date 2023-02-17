ef3c7963bc5ff584f089ab40bcfcc660bc48fe13
Dan Acuna <danieljacuna@gmail.com>
2023-02-17 18:31:37 -0300

    varchar

--- DataBase/models/Diariovirtual.js
+++ DataBase/models/Diariovirtual.js
@@ -9,35 +9,35 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     que_sientes_que_te_preocupa: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
     algo_genera_inseguridad: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
     te_sientes_usado_o_manipulado: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
     cosas_que_te_cuesta_empezar: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
     sueños_por_cumplir: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
     con_quien_cuesta_comunicarse: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
     que_cosa_quieres_y_como_conseguirla: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
     nota: {
-      type: DataTypes.STRING(100000000000),
+      type: DataTypes.STRING(64000),
       allowNull: true,
     },
   };
